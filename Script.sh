946dbdfe44175ca6abab1f7d18c3f337694e82a1
user20112 <devlinpaddock@gmail.com>
2019-05-15 22:30:53 -0400

    adsded a voltss server opnto it
    mlem

--- Script.sh
+++ Script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cd /home/devlin/ServerStuff/Cuwo/ &&
 xterm -hold -e python3 -m cuwo.server &
-cd /home/devlin/ServerStuff/Minecraft/ &&
-xterm -hold -e java -Xmx5G -Xms1G -jar /home/devlin/ServerStuff/Minecraft/server.jar nogui &
+cd /home/devlin/ServerStuff/Voltz_Server_v2.0.4/ &&
+xterm -hold -e java -Xmx10G -Xms1G -jar /home/devlin/ServerStuff/Voltz_Server_v2.0.4/Voltz.jar nogui &
 cd /home/devlin/ServerStuff/HomerSimpson/ &&
-xterm -hold -e java -Xmx7G -Xms5G -jar /home/devlin/ServerStuff/HomerSimpson/forge-1.12.2-14.23.5.2814-universal.jar nogui &
+xterm -hold -e java -Xmx7G -Xms1G -jar /home/devlin/ServerStuff/HomerSimpson/forge-1.12.2-14.23.5.2814-universal.jar nogui &
 cd /home/devlin/ServerStuff/Scripts/ &&
 xterm -hold -e ./Terraria.sh &
 cd /home/devlin/ServerStuff/FTBInteractionsServer/ &&
