65b352d8b5d4aad688d05eafa270c94245b0e8fb
user20112 <devlinpaddock@gmail.com>
2019-07-16 20:13:56 -0400

    removed some old servers onto a copy

--- Script.sh
+++ Script.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 cd /home/devlin/ServerStuff/Cuwo/ &&
 xterm -hold -e python3 -m cuwo.server &
+cd /home/devlin/ServerStuff/FTBInteractionsServer/ &&
+xterm -hold -e ./ServerStart.sh &
 cd /home/devlin/ServerStuff/Voltz_Server_v2.0.4/ &&
 xterm -hold -e java -Xmx10G -Xms1G -jar /home/devlin/ServerStuff/Voltz_Server_v2.0.4/Voltz.jar nogui &
-cd /home/devlin/ServerStuff/HomerSimpson/ &&
-xterm -hold -e java -Xmx7G -Xms1G -jar /home/devlin/ServerStuff/HomerSimpson/forge-1.12.2-14.23.5.2814-universal.jar nogui &
-cd /home/devlin/ServerStuff/Scripts/ &&
-xterm -hold -e ./Terraria.sh &
-cd /home/devlin/ServerStuff/FTBInteractionsServer/ &&
-xterm -hold -e ./ServerStart.sh 
+cd /home/devlin/ServerStuff/Nowhere-1.2-server/forge/ &&
+sudo xterm -hold -e java -Xmx10G -Xms10G -jar /home/devlin/ServerStuff/Nowhere-1.2-server/forge/forge-1.12.2-14.23.5.2808-universal.jar nogui
 
